35d99c9a9adb133af434db86fb9f0b50889f1f78
Emil Knudsen <emilknudsen@Emils-MBP-9.webspeed.dk>
2023-05-18 01:06:17 +0200

    trying new config

--- main.py
+++ main.py
@@ -35,12 +35,12 @@ def move_and_delete_files(filename):
     # Move winner Genome
     if os.path.exists("winner.pkl"):
         src_path = "winner.pkl"
-        dst_path = r"Statistics/Function_Distribution/QuarterFull/New/winner" + str(filename) + ".pkl"
+        dst_path = r"Statistics/Full_Arena/fs_neat/middle_new_config/winner" + str(filename) + ".pkl"
         shutil.move(src_path, dst_path)
 
     # Move stats
     src_path = "fitness_history.csv"
-    dst_path = r"Statistics/Function_Distribution/QuarterFull/New/fitness_history" + str(filename) + ".csv"
+    dst_path = r"Statistics/Full_Arena/fs_neat/middle_new_config/fitness_history" + str(filename) + ".csv"
     shutil.move(src_path, dst_path)
 
     # Remove unnecessary files
@@ -50,20 +50,20 @@ def move_and_delete_files(filename):
 
 
 if __name__ == "__main__":
-    start_mode = StartMode.Winner
+    start_mode = StartMode.New
     start_type = StartType.Single
     sensing_mode = SensingMode.BoxDiff
-    food_distribution = FoodDistribution.QuarterFull
-    draw = False
+    food_distribution = FoodDistribution.Full
+    draw = True
 
     constants = Constants(draw=draw, sensing_mode=sensing_mode, start_mode=start_mode,
                           food_distribution=food_distribution, start_type=start_type)
 
-    create_pop_and_find_winner(constants=constants, rounds_to_run=2000, winner_file='/Users/emilknudsen/Desktop/research/Statistics/Function_Distribution/HalfFull/TrainedOnFullMiddle/winner0.pkl')
-    exit(0)
+    #create_pop_and_find_winner(constants=constants, rounds_to_run=2000, winner_file='')
+    #exit(0)
 
     #for file in glob.glob("Statistics/Function_Distribution/HalfFull/TrainedOnFullMiddle/winner*.pkl"):
-    for i in range(10):
+    for i in range(20):
     #    file_length = len(file) - file.rfind('/')
     #    last_char_in_filename = len(file)-1
 
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     #        counter = file[last_char_in_filename-5] + file[last_char_in_filename-4]
 
         try:
-            create_pop_and_find_winner(constants=constants, rounds_to_run=3000, winner_file='')
+            create_pop_and_find_winner(constants=constants, rounds_to_run=1000, winner_file='')
         finally:
             move_and_delete_files(i)
 
